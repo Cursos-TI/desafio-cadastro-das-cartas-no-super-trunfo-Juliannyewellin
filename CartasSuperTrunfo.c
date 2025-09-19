d4ed2853aaa17325a23be210870d750c8fa968c2
Julianny Ewellin <juliannyewellin@gmail.com>
2025-09-18 22:51:13 -0300

    adicionado novas variáveis na classe

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,24 +1,35 @@
 #include <stdio.h>
 
-int main(){
-    char carta;
-    char estado;
-    char codigo[5];
-    char nome[20];
+int main (){
+
+    int Pernambuco = 47.000;
+    int Ceara = 50.000;
+    char estados[5];
+    char nome[20]; Pernambuco, Ceara;
     int populacao;
-    float area;
-    float pib;
-    int numerodepontosturisticos;
+    float area; Pernambuco = 98.281, Ceara = 148.886,308;
+    float pib; Pernambuco = 228.67, Ceara = 194.88;
+    int numerodepontosturisticos; Pernambuco = 10, Ceara = 16;
+
+    char carta1 = Pernambuco;
+    char carta2 = Ceara;
+    char estado;
 
     printf("Bem Vindo ao Super Trunfo! \n");
-    printf("Digite o numero da sua carta: \n");
-    scanf(" %c", &carta);
-
-    printf("Digite a letra do estado: \n");
-    scanf(" %c", &estado);
-
-    printf("Digite o código da carta: \n");
-    scanf("%s", codigo);
+    printf("Escolha sua carta (Ex: A01): \n");
+    scanf(" %c %c", &carta1, &carta2);
+    if (carta1 == '1', carta1 == 'carta 1')
+    {
+        printf("Sua carta é: Pernambuco. \n");
+    }
+    else if (carta2 == '2', carta2 == 'carta 2')
+    {
+        printf("Sua carta e: Ceará. \n");
+    }
+    else
+    {
+        printf("Escolha invalida.\n");
+    }
 
     printf("Digite o nome da cidade: \n");
     scanf("%s", nome);
@@ -26,16 +37,14 @@ int main(){
     printf("Digite a população: \n");
     scanf("%d", &populacao);
 
-    printf("escreva a área: \n");
+    printf("digite a área: \n");
     scanf("%f", &area);
 
-    printf("Escreva o PIB da localidade: \n");
+    printf("Digite o PIB da localidade: \n");
     scanf("%f", &pib);
 
     printf("Digite o número de pontos turísticos: \n");
     scanf("%d", &numerodepontosturisticos);
-
-
 
     return 0;
 }
